535781b0e8aa1c7eea77b919c55fb3475edf0365
Vitor Andreola <130668298+dev-andreola@users.noreply.github.com>
2023-08-30 16:25:08 -0300

    adicionando placar

--- sketch.js
+++ sketch.js
@@ -1,16 +1,14 @@
 //BOLINHA
-
 let xBolinha = 300;
 let yBolinha = 200;
 
 let diametroBolinha = 22;
 let raioBolinha = diametroBolinha / 2;
 
-let velxBolinha = 5;
-let velyBolinha = 2;
+let velxBolinha = 6;
+let velyBolinha = 6;
 
 //RAQUETE
-
 let colidiu = false;
 
 let xRaquete = 10;
@@ -18,25 +16,38 @@ let yRaquete = 155;
 let alturaRaquete = 90;
 let larguraRaquete = 10;
 
-//START
+// RAQUETE OPONENTE
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+let velyOponente;
 
+// PLACAR
+meusPontos = 0;
+pontosOponente = 0;
+
+
+//START
 function setup() {  
   createCanvas(600, 400);
 }
 
 function draw() {    
-  background(20);
+  background(50);
   mostraBolinha(); 
   movimentaBolinha();
   colisaoBolinhaBorda();
-  mostraRaquete();  
+  mostraRaquete(xRaquete, yRaquete);  
   movimentaMinhaRaquete();
-  //colisaoBolinhaRaquete();
-  colisaoBolinhaRaqueteBiblioteca();
+  colisaoBolinhaRaquete(xRaquete, yRaquete);
+  mostraRaquete(xRaqueteOponente, yRaqueteOponente);
+  movimentaRaqueteOponente();
+  colisaoBolinhaRaquete(xRaqueteOponente, yRaqueteOponente);
+  incluiPlacar();
+  marcaPonto();
 }
 
-//BOLINHA
 
+//BOLINHA
 function mostraBolinha () {
   circle(xBolinha, yBolinha, diametroBolinha);
 }
@@ -50,20 +61,19 @@ function movimentaBolinha () {
 
 function colisaoBolinhaBorda () {
   if (xBolinha > width - raioBolinha ||
-     xBolinha < diametroBolinha / 2){
+     xBolinha < raioBolinha){
     velxBolinha *= -1
   }
     
   if (yBolinha > height - raioBolinha ||
-     yBolinha < diametroBolinha / 2){
+     yBolinha < raioBolinha){
     velyBolinha *= -1
   }
 }
 
 //RAQUETE
-
-function mostraRaquete(){
-  rect(xRaquete, yRaquete, larguraRaquete, alturaRaquete)
+function mostraRaquete(x, y){
+  rect(x, y, larguraRaquete, alturaRaquete)
 }
 
 function movimentaMinhaRaquete(){
@@ -73,19 +83,40 @@ function movimentaMinhaRaquete(){
   if (keyIsDown(DOWN_ARROW)){
     yRaquete += 10;
   }
+
+  yRaquete = constrain(yRaquete, 4, 305)
 }
 
-function colisaoBolinhaRaquete(){
-  if (xBolinha - raioBolinha < xRaquete + larguraRaquete &&
-     yBolinha - raioBolinha < yRaquete + alturaRaquete &&
-     yBolinha + raioBolinha > yRaquete){
+
+
+function colisaoBolinhaRaquete(x, y){
+  colidiu = collideRectCircle(x, y, larguraRaquete, alturaRaquete, xBolinha, yBolinha, diametroBolinha);
+  if (colidiu) {
     velxBolinha *= -1;
   }
 }
 
-function colisaoBolinhaRaqueteBiblioteca(){
-  colidiu = collideRectCircle(xRaquete, yRaquete, larguraRaquete, alturaRaquete, xBolinha, yBolinha, diametroBolinha);
-  if (colidiu) {
-    velxBolinha *= -1;
+//RAQUETE OPONENTE
+function movimentaRaqueteOponente(){
+  velyOponente = yBolinha - yRaqueteOponente - larguraRaquete / 2 - 30;
+  yRaqueteOponente += velyOponente
+
+  yRaqueteOponente = constrain(yRaqueteOponente, 4, 305)
+}
+
+//PONTUAÇÃO
+function incluiPlacar(){
+  fill(255);
+  text(meusPontos, 278, 26);
+  text(pontosOponente, 321, 26)
+}
+
+
+function marcaPonto() {
+  if (xBolinha > 590){
+    meusPontos++
+  }
+  if (xBolinha < 10){
+    pontosOponente++
   }
 }
